0234eba64051dcf9d1cc22e8f6e972d6f1e88c9f
ratnadia <ratnadia6504@gmail.com>
2025-12-10 16:22:02 +0530

    Fix import order in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
+import { PersonalityProvider } from "./contexts/PersonalityContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-import { PersonalityProvider } from "./contexts/PersonalityContext";
 
 root.render(
   <React.StrictMode>
